63a3cba8666e334286f7c5ff8974a64e06c2b473
Hayden Dillon-Lloyd <115908599+rayjohn666@users.noreply.github.com>
2023-02-12 18:14:18 -0800

    images and pulling restuarant data to homepage

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const Restaurant = require("../models/Restaurant");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('homepage', {layout: 'main'});
+
+router.get("/", (req, res) => {
+
+Restaurant.findAll().then(results => {
+    console.log(results);
+    const restaurants = results.map(result => result.get({plain:true}))
+    console.log(restaurants);
+    res.render("homepage", {
+        restaurants: restaurants
+    });
+})
+  
 });
 
-router.get('/signup', (req, res) => {
-    res.render('signup', {layout: 'main'});
+router.get("/signup", (req, res) => {
+  res.render("signup", { layout: "main" });
 });
 
-router.get('/login', (req, res) => {
-    res.render('login', {layout: 'main'});
+router.get("/login", (req, res) => {
+  res.render("login", { layout: "main" });
 });
 
 module.exports = router;
