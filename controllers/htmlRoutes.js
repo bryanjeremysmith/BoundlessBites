10ea05c9ef74c7b2448c3edd8dd78558d94a2eb5
BJ Smith <bryanjeremysmith@gmail.com>
2023-02-13 21:01:09 -0800

    committing for Ryan

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
 
 Restaurant.findAll().then(results => {
-    console.log(results);
+    //console.log(results);
     const restaurants = results.map(result => result.get({plain:true}))
-    console.log(restaurants);
+    //console.log(restaurants);
     res.render("homepage", {
         restaurants: restaurants
     });
@@ -23,5 +23,25 @@ router.get("/signup", (req, res) => {
 router.get("/login", (req, res) => {
   res.render("login", { layout: "main" });
 });
+
+router.post('/'), async (req, res) => {
+  console.log("here i am")
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+}
 
 module.exports = router;
