76690495b2f2dec7aa0fef2fd7124eb44f994ff5
BJ Smith <bryanjeremysmith@gmail.com>
2023-02-14 14:30:34 -0800

    updating the routes and user to handle checking the password

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -27,7 +27,6 @@ router.get("/login", (req, res) => {
 });
 
 router.post('/api/users', async (req, res) => {
-  console.log("here i am")
   try {
     const dbUserData = await User.create({
       username: req.body.username,
@@ -43,6 +42,54 @@ router.post('/api/users', async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+router.post('/api/users/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/api/users/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
   }
 });
 
