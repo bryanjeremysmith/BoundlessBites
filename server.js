f6011c4767ce460490f9fe8fd7f28491fe5a0248
BJ Smith <bryanjeremysmith@gmail.com>
2023-02-15 21:00:22 -0800

    testing

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.use(express.static(path.join(__dirname, 'public/assets')));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
