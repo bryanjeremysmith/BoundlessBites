3fb9dcbc8d93675d40f13f6a96397f1506c0afa5
BJ Smith <bryanjeremysmith@gmail.com>
2023-02-17 10:40:35 -0800

    updating with hooking up the filter to the cards, adding a link that allows you to click on the cards to send to a restaurant page

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,86 +1,72 @@
-var vegetarian = false;
-var vegan = false;
-var glutenFree = false;
-var kosher = false;
-var halal = false;
-var lowSodium = false;
-var pescatarian = false;
+var vegetarian = true;
+var vegan = true;
+var glutenFree = true;
+// var kosher = false;
+// var halal = false;
+// var lowSodium = false;
+// var pescatarian = false;
 
-function isVegetarianHandler() {
-    var value = document.querySelector('#is_vegetarian').checked;
-    console.log(value);
-    if (value) {
-        document.getElementById('vegetarian').setAttribute('style', 'visibility:visible');
+
+function collapseAll(){
+    var elements = document.getElementsByClassName('vegetarian_vegan_gluten_free_class');
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].setAttribute('style', 'visibility:collapse');
     }
-    else {
-        document.getElementById('vegetarian').setAttribute('style', 'visibility:collapse');
+    elements = document.getElementsByClassName('vegetarian_gluten_free_class');
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].setAttribute('style', 'visibility:collapse');
+    }
+    elements = document.getElementsByClassName('vegan_gluten_free_class');
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].setAttribute('style', 'visibility:collapse');
+    }
+    elements = document.getElementsByClassName('vegetarian_vegan_class');
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].setAttribute('style', 'visibility:collapse');
+    }
+    elements = document.getElementsByClassName('vegetarian_class');
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].setAttribute('style', 'visibility:collapse');
+    }
+    elements = document.getElementsByClassName('vegan_class');
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].setAttribute('style', 'visibility:collapse');
+    }
+    elements = document.getElementsByClassName('gluten_free_class');
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].setAttribute('style', 'visibility:collapse');
+    }
+}
+function dietaryCheckBoxClick(){
+    vegetarian = document.querySelector('#is_vegetarian').checked;
+    vegan = document.querySelector('#is_vegan').checked;
+    glutenFree = document.querySelector('#is_gluten_free').checked;
+
+    collapseAll();
+    var elements;
+    if(vegetarian && vegan && glutenFree){
+        elements = document.getElementsByClassName('vegetarian_vegan_gluten_free_class');
+    }else if(vegetarian && glutenFree){
+        elements = document.getElementsByClassName('vegetarian_gluten_free_class');
+    }else if(vegan && glutenFree){
+        elements = document.getElementsByClassName('vegan_gluten_free_class');
+    }else if(vegetarian && vegan){
+        elements = document.getElementsByClassName('vegetarian_vegan_class');
+    }else if(vegetarian){
+        elements = document.getElementsByClassName('vegetarian_class');
+    }else if(vegan){
+        elements = document.getElementsByClassName('vegan_class');
+    }else if(glutenFree){
+        elements = document.getElementsByClassName('gluten_free_class');
+    }
+
+    if (elements != []){
+        for (let index = 0; index < elements.length; index++) {
+            elements[index].setAttribute('style', 'visibility:visible');
+        }
     }
 }
 
-function isVeganHandler() {
-    var value = document.querySelector('#is_vegan').checked;
-    if (value) {
-        vegan = 'visibile';
-    }
-    else {
-        vegan = 'collapse';
-    }
-}
-
-function isGfHandler() {
-    var value = document.querySelector('#is_gluten_free').checked;
-    if (value) {
-        glutenFree = 'visibile';
-    }
-    else {
-        glutenFree = 'collapse';
-    }
-}
-
-function isKosherHandler() {
-    var value = document.querySelector('#is_kosher').checked;
-    if (value) {
-        kosher = true;
-    }
-    else {
-        kosher = false;
-    }
-}
-
-function isHalalHandler() {
-    var value = document.querySelector('#is_halal').checked;
-    if (value) {
-        halal = true;
-    }
-    else {
-        halal = false;
-    }
-}
-
-function isLowSodiumHandler() {
-    var value = document.querySelector('#is_lowsodium').checked;
-    if (value) {
-        lowSodium = true;
-    }
-    else {
-        lowSodium = false;
-    }
-}
-
-function isPescatarianHandler() {
-    var value = document.querySelector('#is_pescatarian').checked;
-    if (value) {
-        pescatarian = true;
-    }
-    else {
-        pescatarian = false;
-    }
-}
-
-document.querySelector('#is_vegetarian').addEventListener('click', isVegetarianHandler);
-document.querySelector('#is_vegan').addEventListener('click', isVeganHandler);
-document.querySelector('#is_gluten_free').addEventListener('click', isGfHandler);
-document.querySelector('#is_kosher').addEventListener('click', isKosherHandler);
-document.querySelector('#is_halal').addEventListener('click', isHalalHandler);
-document.querySelector('#is_lowsodium').addEventListener('click', isLowSodiumHandler);
-document.querySelector('#is_pescatarian').addEventListener('click', isPescatarianHandler);
+document.querySelector('#is_vegetarian').addEventListener('click', dietaryCheckBoxClick);
+document.querySelector('#is_vegan').addEventListener('click', dietaryCheckBoxClick);
+document.querySelector('#is_gluten_free').addEventListener('click', dietaryCheckBoxClick);
